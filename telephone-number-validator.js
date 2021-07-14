a6da00c5c28a0a03a4a844bdc99ed0e3df88771d
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-07-14 00:35:30 +0000

    added the telephone number validator file

--- telephone-number-validator.js
+++ telephone-number-validator.js
@@ -2,4 +2,4 @@ function telephoneCheck(str) {
     return /^(1\s|1)?(\(\d{3}\)|\d{3})(-|\s)?\d{3}(-|\s)?\d{4}$/.test(str);
 }
 
-telephoneCheck("555-555-5555");
+console.log(telephoneCheck("555-555-5555"));
